df7d441836916ed5aa886f7f060d37dd3743c77b
Joshua Kitzrow <1030749@choctawnation.com>
2024-11-04 15:43:18 -0600

    test solitaire

--- solitaireJS.js
+++ solitaireJS.js
@@ -1,20 +1,38 @@
 second = false;
 first = "";
-pId = "";
 function Select(id) {
     if (second) {
-        second = false;
-        Second(id);
+        second = Second(id);
         return null;
     }
-    second = true;
+    else
+    {
+        second = First(id);
+        return null;
+    }
+}
+
+function First(id) {
+    if (document.getElementById(id).className == "base") {
+        return false;
+    }
     first = id;
-    pId = document.getElementById(id).parentElement.id;
-    document.getElementById(id).innerHTML += "First";
+    document.getElementById(id).innerHTML = "<img src='sudoku.png'>First";
+    return true;
 }
 
 function Second(id) {
-    document.getElementById(id).parentElement.appendChild(document.getElementById(first));
-    document.getElementById(pId).removeChild(document.getElementById(first));
-    document.getElementById(id).innerHTML += "Second";
+    if (document.getElementById(first).contains(document.getElementById(id)) || document.getElementById(id).id == first) {
+        console.log("Failure");
+        return true;
+    }
+    prnt = document.getElementById(id).parentNode;
+    insertChild = document.getElementById(first).nextSibling;
+    while (document.getElementById(first).nextSibling != null) {
+        prnt.appendChild(document.getElementById(first).nextSibling);
+    }
+    prnt.insertBefore(document.getElementById(first), insertChild);
+    document.getElementById(id).innerHTML = "<img src='sudoku.png'>Second";
+    first = "";
+    return false;
 }
